e4b799db67282d1539ec0a7a027abc58d9625ed9
Miriam K. Wolff <miriamkwolff@hotmail.com>
2026-02-07 21:32:12 +0100

    Unit linux test fix

--- Sources/LoopAlgorithmToPython/LoopAlgorithmToPython.swift
+++ Sources/LoopAlgorithmToPython/LoopAlgorithmToPython.swift
@@ -23,22 +23,31 @@ private func signalHandler(signal: Int32) {
 }
 
 #if os(Linux) || os(Windows)
-    // Non-Apple platforms use raw Double
-    public typealias AlgorithmValue = Double
-#else
-    // Apple platforms use LoopQuantity
-    public typealias AlgorithmValue = LoopQuantity
-#endif
+// If the Linux library expects LoopQuantity but doesn't provide it,
+// we define a compatible version here.
+public struct LoopQuantity: Codable {
+    public let unit: LoopUnit
+    public let doubleValue: Double
 
-extension AbsoluteScheduleValue where T == AlgorithmValue {
-    static func universal(startDate: Date, endDate: Date, value: Double, unit: String) -> AbsoluteScheduleValue<AlgorithmValue> {
-        #if os(Linux) || os(Windows)
-            return AbsoluteScheduleValue(startDate: startDate, endDate: endDate, value: value)
-        #else
-            return AbsoluteScheduleValue(startDate: startDate, endDate: endDate, value: LoopQuantity(unit: LoopUnit(from: unit), doubleValue: value))
-        #endif
+    public init(unit: LoopUnit, doubleValue: Double) {
+        self.unit = unit
+        self.doubleValue = doubleValue
+    }
+
+    public func doubleValue(for unit: LoopUnit) -> Double {
+        return doubleValue
     }
 }
+
+public struct LoopUnit: Codable {
+    public let unitString: String
+    public init(from string: String) { self.unitString = string }
+    public static let gram = LoopUnit(from: "g")
+}
+#endif
+
+// This makes AlgorithmValue always LoopQuantity on ALL platforms
+public typealias AlgorithmValue = LoopQuantity
 
 @_cdecl("initializeExceptionHandler")
 public func initializeExceptionHandler() {
@@ -438,27 +447,25 @@ public func getDynamicCarbsOnBoard(jsonData: UnsafePointer<Int8>?) -> Double {
         let endDate = dateFormatter.date(from: input.inputICE.last!.startAt)!
 
         // This uses the compatibility layer to work on all platforms
-        let carbRatio = [AbsoluteScheduleValue<AlgorithmValue>.universal(
+           let carbRatio = [AbsoluteScheduleValue(
             startDate: startDate,
             endDate: endDate,
-            value: input.carbRatio,
-            unit: "g/U"
+            value: LoopQuantity(unit: LoopUnit(from: "g/U"), doubleValue: input.carbRatio)
         )]
 
-        let isf = [AbsoluteScheduleValue<AlgorithmValue>.universal(
+        let isf = [AbsoluteScheduleValue(
             startDate: startDate,
             endDate: endDate,
-            value: input.sensitivity,
-            unit: "mg/dL/U"
+            value: LoopQuantity(unit: LoopUnit(from: "mg/dL/U"), doubleValue: input.sensitivity)
         )]
+
         let statuses = [carbEntries[0]].map(
             to: inputICE,
             carbRatio: carbRatio,
             insulinSensitivity: isf,
             initialAbsorptionTimeOverrun: 2.0,
             absorptionModel: PiecewiseLinearAbsorption()
-        )
-        // TODO: Add a function for different absorbrionmodels
+        )        // TODO: Add a function for different absorbrionmodels
 
         // The output here is a list of CarbValues with startDate, endDate (equal to startDate), and value (ICE?)
         let carbsOnBoard = statuses.dynamicCarbsOnBoard(
