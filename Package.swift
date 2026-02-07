14c5a4ac0c2c107aa10a2214ebea2583353af307
Miriam K. Wolff <miriamkwolff@hotmail.com>
2026-02-07 23:48:47 +0100

    testing new package.swift condition on os

--- Package.swift
+++ Package.swift
@@ -1,18 +1,22 @@
 // swift-tools-version: 5.10
 import PackageDescription
 
-let package = Package(
-    name: "LoopAlgorithmToPython",
-    // Only apply localization and platforms if NOT on Windows
-    #if !os(Windows)
-    defaultLocalization: "no",
-    platforms: [
+// Define platforms conditionally
+let packagePlatforms: [SupportedPlatform]
+#if os(Windows)
+    packagePlatforms = [] // Windows doesn't need platform-specific tags here
+#else
+    packagePlatforms = [
         .macOS(.v13),
         .iOS(.v15),
         .tvOS(.v15),
         .watchOS(.v8)
-    ],
-    #endif
+    ]
+#endif
+
+let package = Package(
+    name: "LoopAlgorithmToPython",
+    platforms: packagePlatforms,
     products: [
         .library(
             name: "LoopAlgorithmToPython",
