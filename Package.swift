8d7d966c6ff3d995ebdcbab5733ce407bd5cb9e4
Miriam K. Wolff <miriamkwolff@hotmail.com>
2026-02-07 23:43:52 +0100

    Trying to fix windows build

--- Package.swift
+++ Package.swift
@@ -1,19 +1,19 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "LoopAlgorithmToPython",
-    //defaultLocalization: "no",
+    // Only apply localization and platforms if NOT on Windows
+    #if !os(Windows)
+    defaultLocalization: "no",
     platforms: [
         .macOS(.v13),
         .iOS(.v15),
         .tvOS(.v15),
         .watchOS(.v8)
     ],
+    #endif
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "LoopAlgorithmToPython",
             type: .dynamic,
@@ -23,8 +23,6 @@ let package = Package(
         .package(url: "https://github.com/tidepool-org/LoopAlgorithm.git", branch: "main"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "LoopAlgorithmToPython",
             dependencies: ["LoopAlgorithm"]
